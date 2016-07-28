949bb4471eef25c5a5cadb7bd65140e48d76b30b
Jason Yao <jasony.edu@gmail.com>
2016-07-29 04:06:06 +0800

    Fixed class compilation error

--- bootstrap/Main01.java
+++ bootstrap/Main01.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
  * Question link: <UVA Question Link>
  * Valid solution: TODO
  */
-public class Main00
+public class Main01
 {
     public static void main(String args[])
     {
--- special_bootstrap/Z.java
+++ special_bootstrap/Z.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
  * Question link: <UVA Question Link>
  * Valid solution: TODO
  */
-public class Main00
+public class Z
 {
     public static void main(String args[])
     {
